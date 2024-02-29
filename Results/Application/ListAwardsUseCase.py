63316293c81691695d265019fb281c99fd1569df
K-PM <kristellperez34@gmail.com>
2024-02-28 22:50:55 -0600

    update git

--- Results/Application/ListAwardsUseCase.py
+++
@@ -1,6 +0,0 @@
-class ListAwardsUseCase:
-    def __init__(self, award_repo):
-        self.award_repo = award_repo
-
-    def execute(self):
-        return self.award_repo.list_all()
--- Results/Domain/Entities/AAward.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass, field
-import uuid
-
-@dataclass
-class AAward:
-    award_uuid  : str = field(default_factory=uuid.uuid4, init=False)
-    name : str
-    description : str
-    
